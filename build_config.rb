a0baa8f3d2ef4c27de9bac40f4d7746f34921ee5
masahino <masahino@gmail.com>
2026-09-06 11:52:15 +0900

    streamline build_config

--- build_config.rb
+++ build_config.rb
@@ -1,24 +1,36 @@
 MRuby::Build.new do |conf|
-  toolchain :clang
+  conf.toolchain :clang
+
+  conf.enable_debug
+
+  # UTF-8-aware String (String#length / [] / index operate on characters).
+  conf.cc.defines += %w[MRB_UTF8_STRING]
+
   conf.gembox 'default'
 
-  conf.gem github: 'iij/mruby-regexp-pcre' do |gem|
-    gem.skip_test = true
-  end
-  conf.gem github: 'mattn/mruby-iconv' do |gem|
-    gem.skip_test = true
-  end
   conf.gem github: 'masahino/mruby-mrbmacs-lsp'
-  conf.gem github: 'masahino/mruby-lsp-client' do |gem|
-    gem.skip_test = true
+  conf.gem github: 'masahino/mruby-lsp-client' do |g|
+    g.skip_test = true
+  end
+  conf.gem github: 'masahino/mruby-mrbmacs-dap'
+  conf.gem github: 'masahino/mruby-mrbmacs-aichat'
+  conf.gem github: 'masahino/mruby-mrbmacs-agent'
+  conf.gem github: 'masahino/mruby-debug'
+  conf.gem github: 'masahino/mruby-mrbmacs-themes-base16'
+  conf.gem github: 'masahino/mruby-mrbmacs-themes-tomorrow'
+
+  conf.gem github: 'mattn/mruby-iconv' do |g|
+    g.linker.libraries.delete 'iconv' if RUBY_PLATFORM.include?('linux')
+    g.skip_test = true
+  end
+  conf.gem github: 'iij/mruby-regexp-pcre' do |g|
+    g.skip_test = true
   end
 
-  local_scintilla_cocoa =
-    File.expand_path('../mruby-scintilla-cocoa', __dir__)
-  conf.gem local_scintilla_cocoa if File.directory?(local_scintilla_cocoa)
   conf.gem File.expand_path(__dir__)
 
   conf.linker.libraries << 'c++'
-  conf.enable_test
+
   conf.enable_bintest
+  conf.enable_test
 end
